6e4f2bd7e9f761349fc5086f077cd5dcbcfd605e
Prasanna Srinivasan <prasansrini29@gmail.com>
2020-07-24 17:30:15 +0530

    Conversion to Kotlin.

---
+++ app/src/main/java/com/bot/man/model/Constants.kt
@@ -0,0 +1,7 @@
+package com.bot.man.model
+
+object Constants {
+    const val APPLICATION_PREFERENCE_NAME = "BOT_PREFS"
+    val DEFAULT_SHARED_STRING_VALUE: String? = null
+    const val DEFAULT_SHARED_BOOLEAN_VALUE = false
+}
